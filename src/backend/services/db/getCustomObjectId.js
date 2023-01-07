7a831df6e763cea277adcbf88b1755f9a0db64f4
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-07 12:44:19 +0100

    (exercise-tracker/services): add custom ObjectId generator
    allows us to prefix the ids to make them easier to distinguish
    (similar to "stripe" which prefix their ids)

---
+++ src/backend/services/db/getCustomObjectId.js
@@ -0,0 +1,8 @@
+const { ObjectId } = require("../../db/dbConnection").Types
+
+function getCustomObjectId(prefix) {
+  const objectId = ObjectId().toString()
+  return `${prefix}_${objectId}`
+}
+
+module.exports = getCustomObjectId
