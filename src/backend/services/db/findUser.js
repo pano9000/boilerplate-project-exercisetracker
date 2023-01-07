03d890dff3868c2f2b6de937d181c32191b4f98b
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-07 22:45:30 +0100

    (exercise-tracker/services): add initial findUser service

---
+++ src/backend/services/db/findUser.js
@@ -0,0 +1,27 @@
+const { UserModel } = require("../../db/").models;
+
+const findUser = {
+
+  async findOne(searchObject) {
+    return await this.find("one", searchObject)
+  },
+
+  async findAll() {
+    return await this.find()
+  },
+
+  async find(mode = "all", searchObject) {
+
+    try {
+      const searchResult = (mode === "one") ? await UserModel.findOne(searchObject).exec() : await UserModel.find().exec();
+      return searchResult
+    }
+    catch(error) {
+      console.log(error)
+      throw new Error("Finding User failed")
+    }
+
+  }
+}
+
+module.exports = findUser
