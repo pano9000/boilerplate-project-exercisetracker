01d7aadf49063486e7cd555705a68bc1a40ca102
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-07 11:54:43 +0100

    (exercise-tracker/db): remove redundant length constraint
    there already  is a `match` constraint that checks for min and max length

--- src/backend/db/schemas/dbUserSchema.js
+++ src/backend/db/schemas/dbUserSchema.js
@@ -10,8 +10,6 @@ const UserSchema = new dbConnection.Schema({
     unique: true,
     lowercase: true,
     trim: true,
-    minLength: 3,
-    maxLength: 20,
     match: /^[0-9a-z-_]{3,20}$/
   },
 })
