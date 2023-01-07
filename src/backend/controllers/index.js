836408e6c32c0f4e430ee1fe5cec20d8227ed051
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-08 00:05:17 +0100

    (exercise-tracker/router+controllers): add planned future route/controllers commented out still

--- src/backend/controllers/index.js
+++ src/backend/controllers/index.js
@@ -15,6 +15,13 @@ module.exports = {
       get: require("./api/usersGet"),
       post: require("./api/usersPost"),
 
+      /* TODO: future route, to get single user info and also update user info
+      userId: {
+        get: require("./api/userIdGet"),
+        patch: require("./api/userIdPatch")
+      },
+      */
+
       logs: {
         get: require("./api/logsGet")
       },
--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -13,6 +13,12 @@ router.get("/api/users/:_id/logs", controllers.api.users.logs.get)
 router.post("/api/users", controllers.api.users.post )
 router.post("/api/users/:_id/exercises", controllers.api.users.exercises.post)
 
+
+/* future routes
+router.get("/api/users/:_id", controllers.api.users.userId.get)
+router.patch("/api/users/:_id", controllers.api.users.userId.patch)
+*/
+
 //404 Handling
 router.get("*", controllers.unknown.get)
 
