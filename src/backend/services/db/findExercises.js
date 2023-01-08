088935d3e94c9c97ca475fa3e38a61987a768e14
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-08 12:37:10 +0100

    (exercise-tracker/services): add queryOptions to findExercise -> needs some cleaning still

--- src/backend/services/db/findExercises.js
+++ src/backend/services/db/findExercises.js
@@ -6,14 +6,14 @@ const findExercise = {
     return await this.find("one", searchObject)
   },
 
-  async findAll(searchObject) {
-    return await this.find("all", searchObject)
+  async findAll(searchObject, queryOptions) {
+    return await this.find("all", searchObject, queryOptions)
   },
 
-  async find(mode = "all", searchObject) {
+  async find(mode = "all", searchObject, queryOptions) {
 
     try {
-      const searchResult = (mode === "one") ? await ExerciseModel.findOne(searchObject).exec() : await ExerciseModel.find(searchObject).exec();
+      const searchResult = (mode === "one") ? await ExerciseModel.findOne(searchObject).exec() : await ExerciseModel.find(searchObject, null, queryOptions).exec();
       if (searchResult === undefined) {
         throw new Error("Db returned undefined")
       }
