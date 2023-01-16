e04cb86ba832acef577041e50d7aaba942210685
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-16 09:35:20 +0100

    some more frontendwork dumping

--- src/frontend/vanillaJS/scripts/index.js
+++ src/frontend/vanillaJS/scripts/index.js
@@ -1,34 +1,52 @@
+/*
 const section_create = document.getElementById("section_create")
 const section_read = document.getElementById("section_read")
 const section_update = document.getElementById("section_update")
 const section_delete = document.getElementById("section_delete")
+*/
 
+//TODO: make this dynamic
 const sectionElements = {
   create: document.getElementById("section_create"),
   read: document.getElementById("section_read"),
   update: document.getElementById("section_update"),
   delete: document.getElementById("section_delete"),
+  createuser: document.getElementById("section_createuser"),
+  createexercise: document.getElementById("section_createexercise"),
+  readallusers: document.getElementById("section_readallusers"),
+  readoneuser: document.getElementById("section_readoneuser"),
+  readuserexercise: document.getElementById("section_readuserexercise"),
 }
 
+const navbarElements = {
+  main: document.getElementById("navbar-main"),
+  create: document.getElementById("navbar-create"),
+  read: document.getElementById("navbar-read"),
+  update: document.getElementById("navbar-update"),
+  delete: document.getElementById("navbar-delete")
+}
 
-const navbarMain = document.getElementById("navbar-main")
+//const navbarMain = document.getElementById("navbar-main")
 
 const classBtnNavActive = "btn-nav_active";
 //const classBtnNav = "btn-nav"
 
-navbarMain.addEventListener("click", navBarClickHandler)
+navbarElements.main.addEventListener("click", navbarMainClickHandler)
+navbarElements.create.addEventListener("click", navbarMainClickHandler)
+navbarElements.read.addEventListener("click", navbarMainClickHandler)
 
-
-function navBarClickHandler(event) {
+function navbarMainClickHandler(event) {
   const currentActiveElem = event.target
   if (currentActiveElem.matches(".btn-nav")) {
 
     const previousActiveElem = getPreviousActiveElem(event.target.parentElement)
-    previousActiveElem.classList.remove(classBtnNavActive)
-    currentActiveElem.classList.add(classBtnNavActive)
+
     const currentSection = currentActiveElem.id.split("_").pop();
     const previousSection = previousActiveElem.id.split("_").pop();
-
+    console.log([currentSection, previousSection])
+    previousActiveElem.classList.remove(classBtnNavActive)
+    currentActiveElem.classList.add(classBtnNavActive)
+    
     sectionElements[previousSection].classList.remove("section_action_active");
     sectionElements[currentSection].classList.add("section_action_active");
 
