ee40140b8c3bf99adf1b2a331f89d1847eb52787
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-21 15:12:18 +0100

    add basic PostToAPI function

--- src/frontend/vanillaJS/scripts/index.js
+++ src/frontend/vanillaJS/scripts/index.js
@@ -48,3 +48,37 @@ function getElementsList(querySelector, idRegex) {
 
   return selectedElements
 }
+
+
+async function postToAPI(serverUrl, postData) {
+
+  try {
+    const result = await fetch(serverUrl, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams(postData)
+    })
+
+    if (result.ok !== true) {
+      //ideally do some additional error handling for better error message here
+      throw new Error()
+    }
+
+    const apiData = await result.json()
+
+    return apiData
+  }
+
+  catch(error) {
+    console.log(error)
+    throw new Error("Posting to API failed")
+  }
+
+}
+
+function showResponse(element, response) {
+
+}
