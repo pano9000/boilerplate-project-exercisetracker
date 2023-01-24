f08bf0c4736927277595adbb1f103a4470edaca2
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-24 17:55:46 +0100

    update eventHandlers module

--- src/frontend/vanillaJS/scripts/eventHandlers/index.js
+++ src/frontend/vanillaJS/scripts/eventHandlers/index.js
@@ -1,6 +1,4 @@
 module.exports = {
   navbarMainClickHandler: require("./navbarMainClickHandler"),
-  createExerciseFormHandler: require("./createExerciseFormHandler"),
-  createUserFormHandler: require("./createUserFormHandler"),
-  deleteUserFormHandler: require("./deleteUserFormHandler")
+  submitFormHandler: require("./submitFormHandler")
 }
