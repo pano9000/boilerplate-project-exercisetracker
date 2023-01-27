d861970f02b39cc59d83318d3ccb29d94cacefb7
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-27 01:02:59 +0100

    add "mock" deletion function

--- src/frontend/vue/src/components/CreateUser.vue
+++ src/frontend/vue/src/components/CreateUser.vue
@@ -1,16 +1,12 @@
 <template>
   <h2> {{ title }} </h2>
-  <div id="">
-    <input type="text" placeholder="Username">
-    <button @click="submitUser">Add new user</button>
-
-  </div>
-
   <h3>mockup</h3>
   <label for="admin-ui_showentryqty">Show max. entries</label>
-  <select id="admin-ui_showentryqty">
-    <option  v-for="value in [5, 10, 25, 50]" :key="value"> {{value}}</option>
+  <select id="admin-ui_showentryqty" v-model="ui_showentryqty">
+    <option v-for="value in [5, 10, 25, 50]" :key="value"> {{value}}</option>
   </select>
+
+  <p>Showing {{ ui_showentryqty }} entries of total {{ userList.length }}</p>
   <table>
     <thead>
       <tr>
@@ -20,10 +16,13 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="user in userList" :key="user._id">
+      <tr v-for="user in paginatedList" :key="user._id">
         <td>{{ user._id }}</td>
         <td>{{ user.username }}</td>
-        <td>Edit</td>
+        <td>
+          <span>✏️</span>
+          <span @click="delUser(user.username)">❌</span>
+        </td>
       </tr>
       <tr>+</tr>
     </tbody>
@@ -35,12 +34,14 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-  const title = "Create New User"
+  const title = "User List"
   const userList = ref({})
+  const ui_showentryqty = ref(5)
+  const paginatedList = userList
 
-  
   onMounted(  async () => {
     userList.value = await fetchUsers();
+    console.log(userList.value.slice(0, 4))
   })
   userList.value = fetchUsers();
 
@@ -71,6 +72,18 @@ import { ref, onMounted } from 'vue';
     }
     
   }
+
+  async function delUser(username) {
+    if (confirm(`Are you sure you want to delete ${username}`)) {
+      // api call -> delete user
+      // TODO
+
+      // ui-update: -> delete user from list
+      const indexToDelete = userList.value.findIndex( (userEntry) => userEntry.username === username)
+      userList.value.splice(indexToDelete, 1)
+    }
+  }
+
 
 </script>
 
