a61c864b4cc95ec2ed1c325ca0f8c75eda1f4bfd
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-02-20 00:53:18 +0100

    createSearchObject: refactor to fix potential issues

--- src/backend/services/db/createSearchObject.js
+++ src/backend/services/db/createSearchObject.js
@@ -3,16 +3,21 @@ const createSearchObject = {
   exerciseLog(properties) {
     const { userId, filterDateFrom, filterDateTo } = properties;
 
+    const searchObject = {
+      userId
+    };
+
     const date = (() => {
       if (filterDateFrom !== undefined && filterDateTo === undefined) return { $gte: filterDateFrom};
       if (filterDateTo !== undefined && filterDateFrom === undefined) return { $lte: filterDateTo};
       if (filterDateFrom !== undefined && filterDateTo !== undefined) return { $lte: filterDateTo, $gte: filterDateFrom};
     })();
 
-    return { 
-      userId,
-      date
+    if (date !== undefined) {
+      searchObject.date = date;
     }
+
+    return searchObject
   }
 
 }
