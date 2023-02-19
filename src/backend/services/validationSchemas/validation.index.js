e8aa0afb1d59625c8e566b846b70d0efb6663679
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-02-20 00:58:37 +0100

    validationSchema: add logsGetAll to exported index.js

--- src/backend/services/validationSchemas/validation.index.js
+++ src/backend/services/validationSchemas/validation.index.js
@@ -5,6 +5,7 @@ module.exports = {
     usersPost: require("./validation.schema.usersPost"),
     exercisePost: require("./validation.schema.exercisePost"),
     logsGet: require("./validation.schema.logsGet"),
+    logsGetAll: require("./validation.schema.logsGetAll"),
     userIdGet: require("./validation.schema.userIdGet")
   },
   handler: require("./validation.handler")
