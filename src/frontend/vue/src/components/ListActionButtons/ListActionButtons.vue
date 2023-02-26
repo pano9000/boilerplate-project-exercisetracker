d2a4dceff7e884072411a6f94df57bc9411c1e78
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-02-26 15:40:19 +0100

    ListActionButtons: make the Add New text dynamically assignable via props

--- src/frontend/vue/src/components/ListActionButtons/ListActionButtons.vue
+++ src/frontend/vue/src/components/ListActionButtons/ListActionButtons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ui_list-action-buttons">
-    <button type="button" @click="$emit('click-addNew')">➕ Add New</button>
+    <button type="button" @click="$emit('click-addNew')">➕ {{ (textAddNew) ? textAddNew : 'Add New'}}</button>
     <button type="button" @click="$emit('click-selection')">{{ (!hasSelection) ? "Select All" : "Clear Selection" }}</button>
     <button type="button" :disabled="!hasSelection" :class="hasSelection ? 'active' : 'inactive'" @click="$emit('click-delSelected')">❌ Delete Selected</button>
   </div>
@@ -9,7 +9,7 @@
 <script setup>
 
   defineEmits(["click-addNew", "click-selection", "click-delSelected" ])
-  defineProps(["hasSelection"])
+  defineProps(["hasSelection", "textAddNew"])
 
 </script>
 
