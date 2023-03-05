03b0426a2210cef90af263f0988d7bc83e5fec05
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-06 00:21:15 +0100

    UserDetails: remove menu
    menu entries will be separate modal windows

--- src/frontend/vue/src/components/UserDetails/UserDetails.vue
+++ src/frontend/vue/src/components/UserDetails/UserDetails.vue
@@ -1,11 +1,7 @@
 <template>
-  <!-- show user details, + exercise log-->
-  <MenuBar
-    :menuItems="menuItems">
-  </MenuBar>
 
 
-  <section v-if="menuItems.userDetails.visible">
+  <section>
     <h2>User Details</h2>
     <div>
       <label for="userId">User ID</label>
@@ -19,50 +15,13 @@
 
   </section>
 
-  <section v-if="menuItems.userExerciseList.visible">
-    <ExerciseList
-    :currentUser="currentUser.value._id"
-    ></ExerciseList>
-
-  </section>
-
-  <section v-if="menuItems.userCreateExercise.visible">
-    <CreatExercise
-    :currentUser="currentUser.value._id"
-    ></CreatExercise>
-  </section>
-
 </template>
 
 
 
 <script setup>
-  import MenuBar from "../MenuBar.vue";
   import { ref, onBeforeUpdate, onMounted, onUpdated } from "vue";
-  import CreatExercise from "../CreateExercise/CreatExercise.vue";
-  import ExerciseList from "../ExerciseList/ExerciseList.vue";
   const props = defineProps(["currentUser"]);
-
-
-
-  const menuItems = ref({
-    userDetails: {
-      title: "User Details",
-      visible: true,
-      id: "userDetails",
-    },
-    userExerciseList: {
-      title: "User Exercise List",
-      visible: false,
-      id: "userExerciseList"
-    },
-    userCreateExercise: {
-      title: "Create Exercise",
-      visible: false,
-      id: "userCreateExercise"
-    }
-  });
-
 
 
 </script>
