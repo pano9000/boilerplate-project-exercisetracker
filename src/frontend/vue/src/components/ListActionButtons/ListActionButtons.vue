64ed987c3f7ab9fa4c06460efabfa9d614b8391e
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-09 08:42:37 +0100

    fix(vue/ListActionButtons): fixed textAddNew being undefined

--- src/frontend/vue/src/components/ListActionButtons/ListActionButtons.vue
+++ src/frontend/vue/src/components/ListActionButtons/ListActionButtons.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="ui_list-action-btn_wrap">
-    <button v-if="(options.showAdd !== undefined) ? options.showAdd : true" type="button" @click="$emit('clickAddNew')">➕ {{ (textAddNew) ? textAddNew : 'Add New'}}</button>
+    <button v-if="(options.showAdd !== undefined) ? options.showAdd : true" type="button" @click="$emit('clickAddNew')">➕ {{ (options.textAddNew) ? options.textAddNew : 'Add New'}}</button>
     <button v-if="(options.showSelect !== undefined) ? options.showSelect : true" type="button" @click="$emit('clickSelection')">{{ (!hasSelection) ? "Select All" : "Clear Selection" }}</button>
     <button v-if="(options.showDelete !== undefined) ? options.showDelete : true" type="button" :disabled="!hasSelection" :class="hasSelection ? 'ui_list-action-btn_active' : 'ui_list-action-btn_inactive'" @click="$emit('clickDelSelected')">❌ Delete Selected</button>
   </section>
