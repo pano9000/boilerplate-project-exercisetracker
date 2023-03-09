f8d08e7c11894acf380cbf7b9be3d1b3cbacf2d9
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-10 00:45:57 +0100

    vue/apiEndpoints: add getAllExercises, addUser & addExercise

--- src/frontend/vue/src/services/apiEndpoints.js
+++ src/frontend/vue/src/services/apiEndpoints.js
@@ -17,8 +17,22 @@ export async function getUserById(userId) {
     return apiResponse.data
 }
 
+export async function getAllExercises(params) {
+  const apiResponse = await sendToAPI.get(`http://localhost:3002/api/users/logs?limit=${params.limit}&from=${params.dateFrom}&to=${params.dateTo}`)
+  return apiResponse
+}
 
 export async function deleteUserById(userId) {
   const apiResponse = await sendToAPI.delete(`http://localhost:3002/api/users/${userId}`)
+  return apiResponse
+}
+
+export async function addUser(formData) {
+  const apiResponse = await sendToAPI.post(`http://localhost:3002/api/users/`, formData)
+  return apiResponse
+}
+
+export async function addExercise(userId, formData) {
+  const apiResponse = await sendToAPI.post(`http://localhost:3002/api/users/${userId}/exercises`, formData)
   return apiResponse
 }
