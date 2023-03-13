ceb408922eb2acd62973ba0ae5a5befcc7377566
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-13 09:16:10 +0100

    vue/utils: add general submitFormHandler

--- src/frontend/vue/src/services/utils.js
+++ src/frontend/vue/src/services/utils.js
@@ -1,3 +1,38 @@
 export function formValidityCheck(event, refToUpdate) {
   refToUpdate.value = event.target.form.checkValidity();
 }
+
+
+
+export async function submitFormHandler(event, usernameref, apiFunction, apiResponseHandler) {
+  try {
+    event.preventDefault();
+    event.target.disabled = true;
+
+    const form = event.target.form;
+
+    if (form.checkValidity() !== true) {
+      throw new Error("Invalid Data Entered")
+    }
+    const formData = new FormData(form);
+
+    //todo: show some loading spinner?
+
+    const apiResponse = await apiFunction(formData)
+    console.log(apiResponse)
+    apiResponseHandler(apiResponse)
+  
+    if (!apiResponse.statusOK) {
+      throw new Error("Error Server")
+      //TODO: add proper handling
+    }
+    alert("User added")
+    usernameref.value = "";
+    event.target.disabled = false;
+
+  }
+  catch(error) {
+    console.log(error)
+    alert(`something went wrong, ${error.message}`)
+  }
+};
