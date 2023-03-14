3a0f9c806e7bdee96473dcafee102c66f53679e3
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-14 23:59:51 +0100

    vue/CreateUser: rework layout

--- src/frontend/vue/src/components/CreateUser/CreateUser.vue
+++ src/frontend/vue/src/components/CreateUser/CreateUser.vue
@@ -1,58 +1,44 @@
 <template>
-  <form action="" method="post">
-    <h3>Add new User</h3>
-
-    <section>
-      <h4>Username Requirements </h4>
-      <ul class="list_requirements">
-        <li>Allowed characters:
-          <ul>
-            <li>Numbers: <span class="spanhighlight">0-9</span></li>
-            <li>Letters: <span class="spanhighlight">a-z</span> (lowercase)</li>
-            <li>Hyphen: <span class="spanhighlight">-</span></li>
-            <li>Underscore: <span class="spanhighlight">_</span></li>
-          </ul>
-        </li>
-      <li>Length: <span class="spanhighlight">3–30</span> characters</li>
-    </ul>
-    </section>
-
-    <section>
-      <fieldset>
-        <legend>User Information</legend>
-        <label for="input_username">Username:</label>
-        <input
-          id="input_username"
-          name="username"
-          type="text"
-          v-model="username.value"
-          @input="formValidityCheck($event, isValidData)"
-          required
-          pattern="^[a-z0-9_\-]{3,30}$"
-          autocomplete="false"
-        >
-      </fieldset>
-    </section>
-
-    <section>
-      <button 
-        type="submit"
-        :disabled="!isValidData.value"
-        @click="addUserHandler($event, username)"
-        >
-        Add User
-      </button>
-    </section>
+  <form action="" method="post" autocomplete="off">
+    <h3>Add New User</h3>
+    <div class="ui-input_wrap ui-input_wrap_required">
+      <label for="ui-createuser_input-username">
+        <span class="ui-input-label_title">Username</span>
+        <span class="ui-input-label_hint">Please enter the username for the new user</span>
+      </label>
+      <input
+        id="ui-createuser_input-username"
+        name="username"
+        type="text"
+        placeholder=""
+        required
+        pattern="^[a-z0-9_\-]{3,30}$"
+        autocomplete="false"
+        v-model="username.value"
+        @input="formValidityCheck($event, isValidData)"
+      >
+      <span class="ui-input-label_hint ui-input-label_reqs">The username should have a length of minimum 3 and maximum 30 characters.</span>
+      <span class="ui-input-label_hint ui-input-label_reqs"> It and can consist of <span class="spanhighlight">0-9</span> (Numbers), <span class="spanhighlight">a-z</span> (lowercase letters), <span class="spanhighlight">-</span> (minus) and <span class="spanhighlight">_</span> (underscore) characters.
+      </span>
+  </div>
+  <button 
+    type="submit"
+    :disabled="!isValidData.value"
+    @click="submitFormHandler($event, username, addUser)"
+  >
+    Add User
+  </button>
   </form>
-
 </template>
 
 
 <script setup>
 
-  import { addUserHandler } from "./CreateUser.functions";
   import { formValidityCheck } from "../../services/utils"
-  import { ref, reactive } from "vue";
+  import { reactive } from "vue";
+  import { submitFormHandler } from "../../services/utils.js";
+  import { addUser } from "../../services/apiEndpoints";
+
 
   const isValidData = reactive({
     value: false
