fd9a824d0dfdc2ed5c74992f7ab811facaf8e40b
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-14 23:57:11 +0100

    vue/ExerciseFilters: rework layout

--- src/frontend/vue/src/components/ExerciseFilters.vue
+++ src/frontend/vue/src/components/ExerciseFilters.vue
@@ -7,16 +7,17 @@
       <section class="ui-exercise-filter_section">
         <h5>Date Range</h5>
 
-        <div>
-          <label for="filter-dateFrom">Date From</label>
-          <input id="filter-dateFrom" type="date" v-model="exerciseFilters.dateFrom">
-        </div>
+        <div class="ui-exercise-filter-flex_wrap">
+          <div>
+            <label for="filter-dateFrom">From</label>
+            <input id="filter-dateFrom" type="date" v-model="exerciseFilters.dateFrom">
+          </div>
 
-        <div>
-          <label for="filter-dateTo">Date To</label>
-          <input id="filter-dateTo" type="date" v-model="exerciseFilters.dateTo">
+          <div>
+            <label for="filter-dateTo">To</label>
+            <input id="filter-dateTo" type="date" v-model="exerciseFilters.dateTo">
+          </div>
         </div>
-
         <!-- TODO: add funct
         <div>
         Set Date To:
@@ -31,37 +32,40 @@
 
       <section class="ui-exercise-filter_section">
         <h5>Sorting</h5>
-        <div>
-          <label for="filter-sortby">Sort By</label>
-          <select id="filter-sortby" v-model="exerciseFilters.sortBy">
-            <option v-for="sortby in ['TODO']" :key="sortby">{{ sortby }}</option>
-          </select>
-        </div>
+        <div class="ui-exercise-filter-flex_wrap">
 
-        <div> <!-- TODO: replace this with a button -->
-          <label for="filter-sortorder">Sort Order</label>
-          <select id="filter-sortorder" v-model="exerciseFilters.sortOrder">
-            <option v-for="sortOrder in ['Ascending', 'Descending']" :key="sortOrder">{{ sortOrder }}</option>
-          </select>
-        </div>
+          <div>
+            <label for="filter-sortby">Sort By</label>
+            <select id="filter-sortby" v-model="exerciseFilters.sortBy">
+              <option v-for="sortby in ['TODO']" :key="sortby">{{ sortby }}</option>
+            </select>
+          </div>
 
+          <div> <!-- TODO: replace this with a button -->
+            <label for="filter-sortorder">Sort Order</label>
+            <select id="filter-sortorder" v-model="exerciseFilters.sortOrder">
+              <option v-for="sortOrder in ['Ascending', 'Descending']" :key="sortOrder">{{ sortOrder }}</option>
+            </select>
+          </div>
+
+          <div>
+            <label for="filter-limit">Limit</label>
+            <select id="filter-limit" v-model="exerciseFilters.limit">
+              <option v-for="limit in ['Show All', 5, 10, 25, 50]" :key="limit" :value="(limit == 'Show All')? 0 : limit">{{ limit }}</option>
+            </select>
+          </div>
+
+        </div>
       </section>
 
     </div>
     <section class="ui-exercise-filter_section">
-      <h5>as</h5>
-      <div>
-      <label for="filter-limit">Limit</label>
-      <select id="filter-limit" v-model="exerciseFilters.limit">
-        <option v-for="limit in ['Show All', 5, 10, 25, 50]" :key="limit" :value="(limit == 'Show All')? 0 : limit">{{ limit }}</option>
-      </select>
-      </div>
-      <hr>
+
       <button @click="$emit('clickLoadExercises', exerciseFilters)">Load Exercises</button>
     </section>
 
   </form>
-
+  <hr>
 
 </template>
 
@@ -85,6 +89,19 @@ import { ref } from 'vue';
 <style>
 .ui-exercise-filter_wrap {
   margin-bottom: 2rem;
+}
+
+.ui-exercise-filter-flex_wrap {
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+}
+
+.ui-exercise-filter_section h5 {
+  margin: .75rem;
 }
 
 </style>
