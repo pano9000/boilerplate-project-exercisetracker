edda3b020bdbd3c419908b23eda35ea953734b23
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-15 07:57:18 +0100

    vue/apiEndpoints: change getAllUsers format to same as the rest

--- src/frontend/vue/src/services/apiEndpoints.js
+++ src/frontend/vue/src/services/apiEndpoints.js
@@ -1,15 +1,8 @@
 import { sendToAPI } from "./apiService";
 
 export async function getAllUsers() {
-  try {
-    const apiResponse = await sendToAPI.get("http://localhost:3002/api/users") //@TODO: add support for future pagination
-    return apiResponse.data
-  }
-  catch(error) {
-    console.log(error)
-    alert(`something went wrong, ${error.message}`) //@TODO: some better error handling, instead of an alert
-  }
-
+  const apiResponse = await sendToAPI.get("http://localhost:3002/api/users") //@TODO: add support for future pagination
+  return apiResponse.data
 }
 
 export async function getUserById(userId) {
