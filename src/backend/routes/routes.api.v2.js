55b7a103aad87fa1284d83afa64be371a881bec4
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-18 23:57:47 +0100

    express/routes: update require paths for api v2

--- src/backend/routes/routes.api.v2.js
+++ src/backend/routes/routes.api.v2.js
@@ -6,41 +6,41 @@ const validation = require("../services/validationSchemas/validation.index")
 router.get("*", controllers.rateLimiter.get);
 router.post("*", controllers.rateLimiter.post);
 
-router.get("/users", controllers.api.users.get);
+router.get("/users", controllers.api.v2.users.get);
 
 //all user's exercises
 router.get(
   "/users/exercises",
   validation.check.logsGetAll,
   validation.handler,
-  controllers.api.users.logsAll.get)
+  controllers.api.v2.users.logsAll.get)
 
 //single user's exercises
 router.get(
   "/users/:userId/exercises",
   validation.check.logsGet,
   validation.handler,
-  controllers.api.users.logs.get);
+  controllers.api.v2.users.logs.get);
 
 router.post(
   "/users",
   validation.check.usersPost,
   validation.handler,
-  controllers.api.users.post
+  controllers.api.v2.users.post
 );
 
 router.post(
   "/users/:userId/exercises",
   validation.check.exercisePost,
   validation.handler,
-  controllers.api.users.exercises.post
+  controllers.api.v2.users.exercises.post
 );
 
 router.get(
   "/users/:userId",
   validation.check.userIdGet,
   validation.handler,
-  controllers.api.users.userId.get
+  controllers.api.v2.users.userId.get
 );
 
 
@@ -49,12 +49,12 @@ router.delete(
   validation.check.userIdGet,
   validation.handler,
   //authentication check,
-  controllers.api.users.userId.delete
+  controllers.api.v2.users.userId.delete
 );
 
 
 /* future routes
-router.patch("/users/:userId", controllers.api.users.userId.patch)
+router.patch("/users/:userId", controllers.api.v2.users.userId.patch)
 */
 
 //404 Handling
