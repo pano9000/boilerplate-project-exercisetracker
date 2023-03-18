28b59bc6e4e2a4e02f607950a64b0d84b12daa23
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-19 00:34:08 +0100

    express/controllers/v2/logsGet: remove unneeded comments

--- src/backend/controllers/api/v2/logsGet.js
+++ src/backend/controllers/api/v2/logsGet.js
@@ -5,34 +5,6 @@ const createSearchObject = require("../../../services/db/createSearchObject")
 
 async function logs(req, res) {
 
-
-  /*
-    1. validate/sanitize userId
-    1.1 check for `from`, `to` and `limit` url query params
-      1.1.1 `from` and `to` are Dates  in yyyy-mm-dd format, `limit` is int
-      http://localhost:3002/api/users/user_63ba110747f325e76e1d931c/logs?from=2022-02-05&to=2022-12-31&limit=100
-    2. search db for exercises with `userId` = `userId from params`
-    (3. search db for user with `_id` = `userId from params`) -> potentially not needed
-    3. create new response object
-      1. map function exercise searchREsult -> get `description, duration, date` from each exercise doc and push into log array as object
-      2. add username and _id ("USER ID") to response
-      3. add count property -> log.length
-
-  */
-  /*
-    expected output by FCC
-    {
-      username: "fcc_test",
-      count: 1,
-      _id: "5fb5853f734231456ccb3b05", //USER ID
-      log: [{
-        description: "test",
-        duration: 60,
-        date: "Mon Jan 01 1990",
-      }]
-    }
-  */
-
   try {
 
     const { userId } = req.params
@@ -41,10 +13,6 @@ async function logs(req, res) {
     }
 
     const { from: filterDateFrom, to: filterDateTo, limit: filterQty, sort: optionSort } = req.query
-
-    // validate -> limit needs to be int, if not -> set to undefined
-    // validate -> sort needs to be asc or desc -> alse set to undefined
-    // validate -> date needs to be date format -> else set to undefined
 
     const findUserResult = await findUser.findOne({ _id: userId })
 
@@ -65,13 +33,14 @@ async function logs(req, res) {
     const response = {
       username: findUserResult.username,
       count: findResult.length,
-      _id: findUserResult._id,
+      user_id: findUserResult._id,
       log: findResult.map(result => {
-        const { description, duration, date } = result;
+        const { description, duration, date, _id } = result;
         return { 
+          exercise_id: _id,
           description, 
           duration, 
-          date: (new Date(date)).toDateString()
+          date: date
         }
       })
     }
