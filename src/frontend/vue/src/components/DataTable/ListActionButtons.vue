3ed338188479f246ab8aa594c5e18543a7357dd8
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-20 14:29:33 +0100

    vue/ListActionButtons: add TablerIcons

--- src/frontend/vue/src/components/DataTable/ListActionButtons.vue
+++ src/frontend/vue/src/components/DataTable/ListActionButtons.vue
@@ -1,13 +1,40 @@
 <template>
   <section class="ui_list-action-btn_wrap">
-    <button v-if="(options.showAdd !== undefined) ? options.showAdd : true" type="button" @click="$emit('clickAddNew')">➕ {{ (options.textAddNew) ? options.textAddNew : 'Add New'}}</button>
-    <button v-if="(options.showSelect !== undefined) ? options.showSelect : true" type="button" @click="$emit('clickSelection')">{{ (!hasSelection) ? "Select All" : "Clear Selection" }}</button>
-    <button v-if="(options.showDelete !== undefined) ? options.showDelete : true" type="button" :disabled="!hasSelection" :class="hasSelection ? 'ui_list-action-btn_active' : 'ui_list-action-btn_inactive'" @click="$emit('clickDelSelected')">❌ Delete Selected</button>
+
+    <button v-if="(options.showAdd !== undefined) ? options.showAdd : true"
+      type="button" 
+      @click="$emit('clickAddNew')"
+    >
+      <IconPlus></IconPlus>
+      {{ (options.textAddNew) ? options.textAddNew : 'Add New'}}
+    </button>
+
+    <button v-if="(options.showSelect !== undefined) ? options.showSelect : true" 
+      type="button"
+      @click="$emit('clickSelection')"
+    >
+      <IconSquareCheck v-if="!hasSelection"></IconSquareCheck>
+      <IconSquareOff v-else></IconSquareOff> 
+      {{ (!hasSelection) ? "Select All" : "Clear Selection" }}
+    </button>
+
+    <button 
+      v-if="(options.showDelete !== undefined) ? options.showDelete : true" 
+      type="button" 
+      :disabled="!hasSelection" 
+      :class="hasSelection ? 'ui_list-action-btn_active' : 'ui_list-action-btn_inactive'" 
+      @click="$emit('clickDelSelected')"
+    >
+      <IconX></IconX>
+      Delete Selected
+    </button>
+
   </section>
 </template>
 
 <script setup>
 
+  import { IconSquareCheck, IconSquareOff, IconPlus, IconX } from '@tabler/icons-vue';
   defineEmits(["clickAddNew", "clickSelection", "clickDelSelected" ]);
   defineProps(["hasSelection", "options"]);
 
@@ -17,4 +44,10 @@
 .ui_list-action-btn_wrap {
   padding: 1rem;
 }
+
+.ui_list-action-btn_wrap button {
+  display: inline-flex;
+  align-items: center;
+}
+
 </style>
