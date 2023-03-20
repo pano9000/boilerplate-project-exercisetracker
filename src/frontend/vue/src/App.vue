be1fe6ff66a9e50b845fa69d269bb007416aefb5
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-20 16:57:34 +0100

    vue/App: cleanup unused code

--- src/frontend/vue/src/App.vue
+++ src/frontend/vue/src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <!-- User UI
-  <div>
-    <label>Select UI</label>
-    <select v-model="active_ui">
-      <option v-for="ui in ['Admin', 'User']" :key="ui" :value="ui.toLowerCase()">{{ ui }}</option>
-    </select>
-  </div>
--->
+  
 
   <section v-if="active_ui==='admin'">
     <UiAdmin>
@@ -25,19 +18,8 @@
   import UiAdmin from "./UiAdmin.vue"
   import UiUser from "./UiUser.vue"
   import { ref, onMounted } from 'vue';
-  //import { fetchUsers } from "./components/UserList/UserList.functions";
 
   const active_ui = ref("admin")
-
-  //todo: remove from final / only for dev
-  //  const userList = ref([]);
-  /*  onMounted(  async () => {
-      //todo: remove from final / only for dev
-      //userList.value = await fetchUsers();
-    })
-  */
-
-
 
 </script>
 
