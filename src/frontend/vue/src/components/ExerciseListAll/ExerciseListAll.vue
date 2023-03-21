190b3f49c4e25e203e20db50081235af998dab84
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-21 11:55:36 +0100

    vue/ExercistListAll: add deleteExerciseHandler

--- src/frontend/vue/src/components/ExerciseListAll/ExerciseListAll.vue
+++ src/frontend/vue/src/components/ExerciseListAll/ExerciseListAll.vue
@@ -24,11 +24,11 @@
     :dataKeyId="'_id'"
     @update-current-item="(newValue) => updateValue(newValue, currentExercise)"
     @update-selected-items="(newValue) => updateValue(newValue, selectedExercises)"
-    @click-del-selected="deleteExerciseHandler(selectedExercises.value, userList.value)"
+    @click-del-selected="deleteExerciseHandler(selectedExercises.value, exerciseList.value)"
   >
     <template v-slot:actionMenuEntries>
       <li @click="uiVisibilityHandler(uiVisibility, 'exerciseDetails')" title="Edit Exercise"><IconPencil></IconPencil> Edit Exercise</li>
-      <li @click="deleteExerciseHandler([currentExercise.value], userList.value)" title="Delete Exercise"><IconX></IconX> Delete Exercise</li>
+      <li @click="deleteExerciseHandler([currentExercise.value], exerciseList.value)" title="Delete Exercise"><IconX></IconX> Delete Exercise</li>
     </template>
 
   </DataTable>
@@ -45,7 +45,7 @@
 
 <script setup>
   import { ref, reactive, onMounted, computed } from "vue";
-  import { getAllExercises } from "../../services/apiEndpoints";
+  import { getAllExercises, deleteExerciseById } from "../../services/apiEndpoints";
   import DataTable from "../DataTable/DataTable.vue";
   import ExerciseFilters from "../ExerciseFilters.vue";
   import { uiVisibilityHandler, updateValue } from "../../services/utils";
@@ -53,13 +53,14 @@
 
 
   const title = "User Exercise Logs";
-  const userList = reactive({ value: [] });
+
   const currentExercise = reactive({ value: {} });
   const selectedExercises = reactive({ value: [] });
 
   const exerciseCount = reactive({
     value: ""
   });
+
   const exerciseList = reactive({
     value: []
   });
@@ -89,10 +90,29 @@
       }
   }
 
-  async function deleteExerciseHandler() {
-    //#TODO
-  }
+  async function deleteExerciseHandler(selectedExercisesP, exerciseListP) {
 
+    const confirmMessage = (!(selectedExercisesP.length > 1)) ?
+    `Are you sure you want to delete the exercise '${selectedExercisesP[0]._id}' of '${selectedExercisesP[0].userId}'` :
+    `Are you sure you want to delete the ${selectedExercisesP.length} selected users?`;
+    //TODO: replace by some fancy "popup"
+    if (confirm(confirmMessage)) {
+
+      await Promise.all(
+        selectedExercisesP.map(async (selectedItem) => {
+          const indexToDelete = exerciseListP.findIndex( (listEntry) => listEntry["_id"] === selectedItem["_id"]);
+          
+          const { _id: exercIdForDeletion, userId } = exerciseListP[indexToDelete];
+          //console.log(exercIdForDeletion, userId, indexToDelete, exerciseListP)
+          const apiResponse = await deleteExerciseById(userId, exercIdForDeletion);
+          if (apiResponse.statusOK) {
+            exerciseListP.splice(indexToDelete, 1);
+          }
+        })
+      );
+
+    }
+  }
 
 
 
