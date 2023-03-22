3e5b8006a057d9b0ad87384bf84bb31ca3dc4e8f
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-22 13:03:13 +0100

    vue/CreateExercise: rework the form handling

--- src/frontend/vue/src/components/CreateExercise/CreateExercise.vue
+++ src/frontend/vue/src/components/CreateExercise/CreateExercise.vue
@@ -3,6 +3,7 @@
   <form class="ui-createexercise_form ui-form">
     <div class="ui-input_wrap ui-input_wrap_required">
       <label for="ui-createexercise_input-userid">User Id</label>
+
       <input
         id="ui-createexercise_input-userid"
         type="text"
@@ -11,7 +12,7 @@
         required
         readonly
         pattern="^[a-zA-Z]{1,5}_[0-9a-f]{24}$"
-        v-model="createExerciseForm.userId"
+        v-model="createExerciseForm.userId.value"
       >
     </div>
 
@@ -20,23 +21,25 @@
         <span class="ui-input-label_title">Description</span>
         <span class="ui-input-label_hint">Please enter a description for the exercise</span>
       </label>
-      <InputStatusIcon
-        :isValidData="isValidData.value"
-        :required="true"
-        :inputElemRef="inputDescription"
-      >
-      </InputStatusIcon>
-      <input 
-        ref="inputDescription"
-        type="text"
-        name="description"
-        placeholder=""
-        required
-        minlength="3"
-        maxlength="50"
-        v-model="createExerciseForm.description"
-        @input="formValidityCheck($event, isValidData)"
-      >
+
+      <div class="ui-input_line">
+        <InputStatusIcon
+          :isValid="createExerciseForm.description.valid"
+          :required="createExerciseForm.description.required"
+        >
+        </InputStatusIcon>
+        <input 
+          type="text"
+          name="description"
+          placeholder=""
+          required
+          minlength="3"
+          maxlength="50"
+          v-model="createExerciseForm.description.value"
+          @input="inputHandler($event, 'description', createExerciseForm)"
+        >
+
+      </div>
       <span class="ui-input-label_hint">Format: Minimum Length 3, Maximum Length 50</span>
     </div>
 
@@ -46,24 +49,26 @@
         <span class="ui-input-label_hint">Please enter the duration of the exercise in minutes</span>
       </label>
 
-      <InputStatusIcon
-        :isValidData="isValidData.value"
-        :required="false"
-        :inputElemRef="inputDuration"  
-      >
-      </InputStatusIcon>
-      <input
-        ref="inputDuration"
-        id="ui-createexercise_input-duration"
-        type="number"
-        name="duration"
-        placeholder=""
-        required
-        min="1"
-        max="999"
-        v-model="createExerciseForm.duration"
-        @input="formValidityCheck($event, isValidData)"
-      >
+      <div class="ui-input_line">
+        <InputStatusIcon
+          :isValid="createExerciseForm.duration.valid"
+          :required="createExerciseForm.duration.required"
+        >
+        </InputStatusIcon>
+
+        <input
+          id="ui-createexercise_input-duration"
+          type="text"
+          name="duration"
+          required
+          placeholder=""
+          pattern="\d{1,3}"
+          v-model="createExerciseForm.duration.value"
+          @input="inputHandler($event, 'duration', createExerciseForm)"
+        >
+
+      </div>
+
       <span class="ui-input-label_hint ui-input-label_reqs">Format: A Number between 1 and 999</span>
     </div>
 
@@ -72,42 +77,84 @@
         <span class="ui-input-label_title">Date</span>
         <span class="ui-input-label_hint">Please enter the date of the exercise</span>
       </label>
-      <input
-        id="ui-createexercise_input-date"
-        type="date"
-        name="date"
-        placeholder=""
-        required
-        v-model="createExerciseForm.date"
-        @input="formValidityCheck($event, isValidData)"
-      >
+
+      <div class="ui-input_line">
+        <InputStatusIcon
+          :isValid="createExerciseForm.date.valid"
+          :required="createExerciseForm.date.required"
+        >
+        </InputStatusIcon>
+
+        <input
+          id="ui-createexercise_input-date"
+          type="date"
+          name="date"
+          placeholder=""
+          required
+          v-model="createExerciseForm.date.value"
+          @input="inputHandler($event, 'date', createExerciseForm)"
+        >
+      </div>
+
     </div>
     <button 
       type="submit" 
       @click.prevent="submitFormHandler($event, createExerciseForm, addExercise)"
-      :disabled="!isValidData.value"
+      :disabled="!isValidData"
     >Create Exercise</button>
   </form>
 </template>
 
 <script setup>
-  import { ref, reactive } from "vue";
+  import { ref, reactive, computed } from "vue";
   import { submitFormHandler, formValidityCheck } from "../../services/utils";
   import { addExercise } from "../../services/apiEndpoints";
   import InputStatusIcon from "../Input-StatusIcon.vue";
   const props = defineProps(["currentUser"]);
 
-  const inputDescription = ref(null);
-  const inputDuration = ref(null);
-
   const createExerciseForm = reactive( {
-    userId: props.currentUser.value._id,
-    description: "",
-    duration: "",
-    date: new Date().toISOString().slice(0,10),
+    userId: {
+      value: props.currentUser.value._id,
+      valid: true,
+      required: true,
+    },
+    description: {
+      value: "",
+      valid: null,
+      required: true,
+    },
+    duration: {
+      value: "",
+      valid: null,
+      required: false
+    },
+    date: {
+      value: new Date().toISOString().slice(0,10),
+      valid: true,
+      required: true,
+    }
   });
 
-  const isValidData = reactive( { value: false } );
+
+
+  const isValidData = computed(() => {
+    for (let item in createExerciseForm) {
+      if (createExerciseForm[item]["valid"] !== true) {
+        return false
+      };
+    }
+    return true
+   });
+
+  function getInputStatus(elem, inputValue) {
+    return (inputValue === "") ? null : elem?.validity?.valid
+  }
+
+  function inputHandler(event, item, statusObj) {
+    statusObj[item].valid = getInputStatus(event.target, statusObj[item]["value"])
+  }
+
+
 
 </script>
 
@@ -122,7 +169,6 @@
   width: 100%;
 }
 
-
 .ui-input_wrap label {
   font-weight: 600;
   font-size: 1rem;
@@ -131,12 +177,17 @@
 .ui-input-label_hint {
   font-weight: 400;
   font-size: 0.8rem;
+  line-height: 1rem;
   display: block;
 }
 
 .ui-input_wrap_required .ui-input-label_title::after {
   content: "*";
   font-size: 1rem;
+}
+
+.ui-input_line {
+  position: relative;
 }
 
 </style>
