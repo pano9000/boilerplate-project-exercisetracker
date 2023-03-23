0297fd8acd3757d8ae6c5e54e0959bfa73f203a6
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-23 17:45:59 +0100

    vue/CreateUser: use the new form style
    TODO: need to abstract this away and turn the form thing into a reusable component or similar

--- src/frontend/vue/src/components/Forms/CreateUser/CreateUser.vue
+++ src/frontend/vue/src/components/Forms/CreateUser/CreateUser.vue
@@ -1,30 +1,38 @@
 <template>
-  <form action="" method="post" autocomplete="off">
-    <h3>Add New User</h3>
+  <h3>Add New User</h3>
+  <form class="ui-createexercise_form ui-form" autocomplete="off">
     <div class="ui-input_wrap ui-input_wrap_required">
       <label for="ui-createuser_input-username">
         <span class="ui-input-label_title">Username</span>
         <span class="ui-input-label_hint">Please enter the username for the new user</span>
       </label>
-      <input
-        id="ui-createuser_input-username"
-        name="username"
-        type="text"
-        placeholder=""
-        required
-        pattern="^[a-z0-9_\-]{3,30}$"
-        autocomplete="false"
-        v-model="username.value"
-        @input="formValidityCheck($event, isValidData)"
-      >
-      <span class="ui-input-label_hint ui-input-label_reqs">The username should have a length of minimum 3 and maximum 30 characters.</span>
-      <span class="ui-input-label_hint ui-input-label_reqs"> It and can consist of <span class="spanhighlight">0-9</span> (Numbers), <span class="spanhighlight">a-z</span> (lowercase letters), <span class="spanhighlight">-</span> (minus) and <span class="spanhighlight">_</span> (underscore) characters.
-      </span>
+
+      <div class="ui-input_line">
+
+        <InputStatusIcon
+          :isValid="createUserForm.username.valid"
+          :required="createUserForm.username.required"
+        >
+        </InputStatusIcon>
+
+        <input
+          id="ui-createuser_input-username"
+          name="username"
+          type="text"
+          placeholder=""
+          required
+          pattern="^[a-z0-9_\-]{3,30}$"
+          autocomplete="false"
+          v-model="createUserForm.username.value"
+          @input="inputHandler($event, 'username', createUserForm)"
+        >
+        <span class="ui-input-label_hint ui-input-label_reqs">The username should have a length of minimum 3 and maximum 30 characters. It and can consist of <span class="spanhighlight">0-9</span> (Numbers), <span class="spanhighlight">a-z</span> (lowercase letters), <span class="spanhighlight">-</span> (minus) and <span class="spanhighlight">_</span> (underscore) characters.</span>
+      </div>
   </div>
   <button 
     type="submit"
-    :disabled="!isValidData.value"
-    @click="submitFormHandler($event, username, addUser)"
+    :disabled="!isValidData"
+    @click="submitFormHandler($event, createUserForm, addUser)"
   >
     Add User
   </button>
@@ -33,34 +41,43 @@
 
 
 <script setup>
+  import { reactive, computed } from "vue";
+  import InputStatusIcon from "../../Input-StatusIcon.vue";
 
-  import { formValidityCheck } from "../../../services/utils"
-  import { reactive } from "vue";
+  import { ReactiveFormItem } from "../../../services/utils"
   import { submitFormHandler } from "../../../services/utils.js";
   import { addUser } from "../../../services/apiEndpoints";
 
-
-  const isValidData = reactive({
-    value: false
+  const createUserForm = reactive( {
+    username: ReactiveFormItem(),
   });
 
-  const username = reactive({
-    value: ""
-  });
+  const isValidData = computed(() => {
+    for (let item in createUserForm) {
+      if (createUserForm[item]["valid"] !== true) {
+        return false
+      };
+    }
+    return true
+   });
+
+   function getInputStatus(elem, inputValue) {
+    return (inputValue === "") ? null : elem?.validity?.valid
+  }
+
+  function inputHandler(event, item, statusObj) {
+    statusObj[item].valid = getInputStatus(event.target, statusObj[item]["value"])
+  }
+
 
 </script>
 
 <style>
 
 .spanhighlight {
-  background-color: honeydew;
+  background-color: #f1f1f1;
   padding: .25rem;
   border-radius: .25rem;
-  box-shadow: var(--box-shadow);
-}
-
-.list_requirements {
-  text-align: left;
 }
 
 </style>
