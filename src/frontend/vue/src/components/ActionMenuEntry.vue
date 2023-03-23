827b7fb0fdd20f8accf5056fea2db938f125f6c0
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-23 19:36:24 +0100

    vue/ActionMenuEntry: add new "sub"-component

---
+++ src/frontend/vue/src/components/ActionMenuEntry.vue
@@ -0,0 +1,15 @@
+<template>
+  <li
+    tabindex="0" 
+    @click="$emit('actionMenuEvent')"
+    @keyup.enter="$emit('actionMenuEvent')"
+    @keyup.space="$emit('actionMenuEvent')"
+  >
+    <slot></slot>
+  </li>
+
+</template>
+
+<script setup>
+
+</script>
