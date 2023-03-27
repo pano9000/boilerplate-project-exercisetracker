218ccf95fac3e03f3db5e244e4222fe9a620b3d1
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-27 23:32:38 +0200

    express/db: add an idPrefix prop to the dbModels
    will be used later for new createDoc service

--- src/backend/db/dbModels.js
+++ src/backend/db/dbModels.js
@@ -1,8 +1,14 @@
 const dbConnection = require("./dbConnection");
 const dbSchemas = require("./schemas")
 
-const UserModel = dbConnection.model("UserDoc", dbSchemas.UserSchema);
-const ExerciseModel = dbConnection.model("ExerciseDoc", dbSchemas.ExerciseSchema);
+const UserModel = { 
+  model: dbConnection.model("UserDoc", dbSchemas.UserSchema),
+  idPrefix: "user"
+};
 
+const ExerciseModel = {
+  model: dbConnection.model("ExerciseDoc", dbSchemas.ExerciseSchema),
+  idPrefix: "exerc"
+};
 
 module.exports = { UserModel, ExerciseModel }
