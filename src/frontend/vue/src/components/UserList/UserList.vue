6cf3054416eab19f4cab999b3df0f3426ed499c8
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-29 08:58:10 +0200

    vue/UserList: fix spacing/indentation

--- src/frontend/vue/src/components/UserList/UserList.vue
+++ src/frontend/vue/src/components/UserList/UserList.vue
@@ -1,77 +1,76 @@
 <template>
   <h2> {{ title }} </h2>
 
-
   <Transition mode="out-in">
 
-  <LoadingSpinner v-if="isLoading">
-  </LoadingSpinner>
-  <section v-else>
+    <LoadingSpinner v-if="isLoading">
+    </LoadingSpinner>
+    <section v-else>
 
-<DataTable
-  :table-options="{showSelection: true, showAction: true}"
-  :list-action-buttons-options="{showBottom: true, showTop: false, textAddNew: 'Add New User'}"
-  :paginationbar-options="{allowSelection: true, showTop: true, showBottom: false}"
-  :table-headings="['UserId', 'Username']"
-  :data-list="userList"
-  :data-keys="['_id', 'username']"
-  :data-key-id="'_id'"
-  @update-current-item="(newValue) => updateValue(newValue, currentUser)"
-  @update-selected-items="(newValue) => updateValue(newValue, selectedUsers)"
-  @click-add-new="uiVisibility.value.createUser = true"
-  @click-del-selected="deleteUserHandler(selectedUsers.value, userList.value)"
->
-  <template v-slot:actionMenuEntries>
-    <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'userDetails')">
-      <IconPencil></IconPencil>
-      Edit User
-    </ActionMenuEntry>
+      <DataTable
+        :table-options="{showSelection: true, showAction: true}"
+        :list-action-buttons-options="{showBottom: true, showTop: false, textAddNew: 'Add New User'}"
+        :paginationbar-options="{allowSelection: true, showTop: true, showBottom: false}"
+        :table-headings="['UserId', 'Username']"
+        :data-list="userList"
+        :data-keys="['_id', 'username']"
+        :data-key-id="'_id'"
+        @update-current-item="(newValue) => updateValue(newValue, currentUser)"
+        @update-selected-items="(newValue) => updateValue(newValue, selectedUsers)"
+        @click-add-new="uiVisibility.value.createUser = true"
+        @click-del-selected="deleteUserHandler(selectedUsers.value, userList.value)"
+      >
+        <template v-slot:actionMenuEntries>
+          <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'userDetails')">
+            <IconPencil></IconPencil>
+            Edit User
+          </ActionMenuEntry>
 
-    <ActionMenuEntry @action-menu-event="deleteUserHandler([currentUser.value], userList.value)">
-      <IconX></IconX>
-      Delete User
-    </ActionMenuEntry>
+          <ActionMenuEntry @action-menu-event="deleteUserHandler([currentUser.value], userList.value)">
+            <IconX></IconX>
+            Delete User
+          </ActionMenuEntry>
 
-    <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'createExercise')">
-      <IconPlus></IconPlus>
-      Add Exercise
-    </ActionMenuEntry>
+          <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'createExercise')">
+            <IconPlus></IconPlus>
+            Add Exercise
+          </ActionMenuEntry>
 
-    <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'exerciseLog')">
-      <IconListDetails></IconListDetails>
-      Show Exercise Log
-    </ActionMenuEntry>
-  </template>
+          <ActionMenuEntry @action-menu-event="uiVisibilityHandler(uiVisibility, 'exerciseLog')">
+            <IconListDetails></IconListDetails>
+            Show Exercise Log
+          </ActionMenuEntry>
+        </template>
 
-</DataTable>
+      </DataTable>
 
 
-  <div v-if="uiVisibility.value.createUser">
-    <ModalWindow @close-modal="uiVisibility.value.createUser=false">
-      <CreateUser></CreateUser>
-    </ModalWindow>
-  </div>
+      <div v-if="uiVisibility.value.createUser">
+        <ModalWindow @close-modal="uiVisibility.value.createUser=false">
+          <CreateUser></CreateUser>
+        </ModalWindow>
+      </div>
 
-  <div v-if="uiVisibility.value.userDetails">
-    <ModalWindow @close-modal="uiVisibility.value.userDetails = false">
-      <UserDetails
-        :current-user="currentUser"
-      ></UserDetails>
-    </ModalWindow>
-  </div>
+      <div v-if="uiVisibility.value.userDetails">
+        <ModalWindow @close-modal="uiVisibility.value.userDetails = false">
+          <UserDetails
+            :current-user="currentUser"
+          ></UserDetails>
+        </ModalWindow>
+      </div>
 
-  <div v-if="uiVisibility.value.createExercise">
-    <ModalWindow
-      @close-modal="uiVisibility.value.createExercise = false"
-    >
-      <CreateExercise
-        :current-user="currentUser"
-      ></CreateExercise>
-    </ModalWindow>
-  </div>
+      <div v-if="uiVisibility.value.createExercise">
+        <ModalWindow
+          @close-modal="uiVisibility.value.createExercise = false"
+        >
+          <CreateExercise
+            :current-user="currentUser"
+          ></CreateExercise>
+        </ModalWindow>
+      </div>
 
-</section>
-</Transition>
+    </section>
+  </Transition>
 
 </template>
 
