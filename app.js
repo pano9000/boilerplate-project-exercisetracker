c40e8fba6b2f700b7f2c57dd4c2271d9be9b2799
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-29 08:21:17 +0200

    express: enable helmet
    closes #30

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 const cors = require('cors')
 const morgan = require("morgan")
 const routes = require("./src/backend/routes/")
+const helmet = require("helmet");
 
+app.use(helmet());
 app.use(morgan("combined"))
 
 app.use(cors())
