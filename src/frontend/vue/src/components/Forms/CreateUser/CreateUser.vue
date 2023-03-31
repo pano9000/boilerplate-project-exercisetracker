44130eefe6655ef07a9b260814b01790ccdb3f7c
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-03-31 09:10:19 +0200

    vue/CreateUser: add &nbsp; to spanhighlight

--- src/frontend/vue/src/components/Forms/CreateUser/CreateUser.vue
+++ src/frontend/vue/src/components/Forms/CreateUser/CreateUser.vue
@@ -27,10 +27,10 @@
         >
         <span class="ui-input-label_hint ui-input-label_reqs">
           The username should have a length of minimum 3 and maximum 30 characters.
-          It can consist of <span class="spanhighlight">0-9</span> (Numbers),
-          <span class="spanhighlight">a-z</span> (lowercase letters),
-          <span class="spanhighlight">-</span> (minus) and 
-          <span class="spanhighlight">_</span> (underscore) characters.
+          It can consist of <span class="spanhighlight">0-9</span>&nbsp;(Numbers),
+          <span class="spanhighlight">a-z</span>&nbsp;(lowercase letters),
+          <span class="spanhighlight">-</span>&nbsp;(minus) and 
+          <span class="spanhighlight">_</span>&nbsp;(underscore) characters.
         </span>
       </div>
   </div>
