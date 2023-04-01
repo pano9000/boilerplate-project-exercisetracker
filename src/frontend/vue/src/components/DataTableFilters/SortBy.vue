52e46e25f1197f1cd2f8353d8ca2fe59f2459f7a
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-01 20:30:33 +0200

    vue/DataTableFilters: add sortBy component
    moved from ExerciseFilters

---
+++ src/frontend/vue/src/components/DataTableFilters/SortBy.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div>
+    <label for="ui-sortby_input">Sort By</label>
+    <select
+      id="ui-sortby_input"
+      @change="$emit('update:modelValue', $event.target.value)"
+    >
+      <option select v-for="sortBy in sortByOptions" :key="sortBy.value" :value="sortBy.value">{{ sortBy.name }}</option>
+    </select>
+  </div>
+
+</template>
+
+<script setup>
+
+  const props = defineProps(["sortByOptions"])
+
+</script>
+
+
+<style>
+
+</style>
