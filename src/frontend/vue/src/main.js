5202c9502f6d02917cb85775913a58f2f168ed36
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-01 12:09:50 +0200

    vue/router: add first vue-router steps

--- src/frontend/vue/src/main.js
+++ src/frontend/vue/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import UiAdmin from "./UiAdmin.vue"
 
-createApp(App).mount('#app')
+
+const routes = [
+  { path: '/', component: UiAdmin },
+]
+
+const router = createRouter( {
+  history: createWebHashHistory(),
+  routes: routes
+} )
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
