5d86f1f8773ab770ee2c51eb37db92754b8a0da1
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-02 16:57:01 +0200

    vue/DataTableFilters/SortBy: adapt to new  dataTableKeys object format

--- src/frontend/vue/src/components/DataTableFilters/SortBy.vue
+++ src/frontend/vue/src/components/DataTableFilters/SortBy.vue
@@ -5,8 +5,8 @@
     id="ui-sortby_input"
     :value="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
-    >
-    <option select v-for="sortBy in sortByOptions" :key="sortBy.value" :value="sortBy.value">{{ sortBy.name }}</option>
+  >
+    <option select v-for="sortBy in sortByOptions" :key="sortBy.key" :value="sortBy.key">{{ sortBy.name }}</option>
   </select>
 
 </template>
