b2e9ae335d82b24d5f6e925bd210550841e18d65
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-05 00:44:31 +0200

    express/validation: use "skip" validation in  logsGetAllSchema

--- src/backend/services/validationSchemas/validation.schema.logsGetAll.js
+++ src/backend/services/validationSchemas/validation.schema.logsGetAll.js
@@ -6,6 +6,7 @@ const logsGetAllSchema = [
   queryParams.from,
   queryParams.to,
   queryParams.limit,
+  queryParams.skip,
   queryParams.sort,
   queryParams.sortBy([...Object.keys(ExerciseSchema.paths)])
 
