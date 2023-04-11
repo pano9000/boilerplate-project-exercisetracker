87992280bb912db5817d53875d7c9fb2d1d63c01
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-11 09:36:59 +0200

    vue/DataTableFilters/DateRange: add quick date range setting feature
    menu still needs a bit of work

--- src/frontend/vue/src/components/DataTableFilters/DateRange.vue
+++ src/frontend/vue/src/components/DataTableFilters/DateRange.vue
@@ -1,40 +1,64 @@
 <template>
   <div>
     <label for="ui-dateFrom_input">Date From</label>
-    <input id="ui-dateFrom_input" type="date" v-model="dateFrom" @input="$emit('update:dateFrom', dateFrom)">
+    <input id="ui-dateFrom_input" type="date" :value="dateFrom.value" @input="updateDateValue($event, 'update:dateFrom', dateFrom)">
   </div>
 
   <div>
     <label for="ui-dateTo_input">Date To</label>
-    <input id="ui-dateTo_input" type="date" v-model="dateTo" @input="$emit('update:dateTo', dateTo)">
+    <input id="ui-dateTo_input" type="date" :value="dateTo.value" @input="updateDateValue($event, 'update:dateTo', dateTo)">
   </div>
 
-  <!-- TODO: add funct
   <div>
-    Set Date To:
-    <button type="button" @click="quickDateButtonsHandler('D', exerciseFilters)">Today</button>
-    <button type="button" @click="quickDateButtonsHandler('W', exerciseFilters)">This Week</button>
-    <button type="button" @click="quickDateButtonsHandler('M', exerciseFilters)">This Month</button>
-    <button type="button" @click="quickDateButtonsHandler('Y', exerciseFilters)">This Year</button>
+    <button type="button" title="Quick Set Date Range"  aria-label="Quick Set Date Range">
+      <IconCalendarCode></IconCalendarCode>
+    </button>
+    <menu class="actionMenu_menu">
+      <li @click="quickDateButtonsHandler('currentDay')"><IconCalendarCode></IconCalendarCode>Today</li>
+      <li @click.prevent="quickDateButtonsHandler('currentWeek')"><IconCalendarCode></IconCalendarCode>This Week</li>
+      <li @click.prevent="quickDateButtonsHandler('currentMonth')"><IconCalendarCode></IconCalendarCode>This Month</li>
+      <li @click.prevent="quickDateButtonsHandler('currentYear')"><IconCalendarCode></IconCalendarCode>This Year</li>
+      <li @click.prevent="quickDateButtonsHandler('showAll')"><IconCalendarCode></IconCalendarCode>Show All</li>
+    </menu>
+
   </div>
--->
 </template>
 
 <script setup>
-  import { ref } from 'vue';
+  import { reactive } from 'vue';
+  import DateRange from '../../services/DateRange';
+  import { IconCalendarCode } from '@tabler/icons-vue';
 
-  defineProps([
+  const dateRange = new DateRange();
+
+  const props = defineProps([
     "dateFrom",
     "dateTo"
   ]);
 
-  defineEmits([
+  const emit = defineEmits([
     "update:dateFrom",
     "update:dateTo",
   ]);
 
-  const dateFrom = ref()
-  const dateTo = ref((new Date()).toISOString().slice(0,10))
+  const dateFrom = reactive({});
+  const dateTo = reactive({});
+
+
+  function quickDateButtonsHandler(mode, dateFromP = dateFrom, dateToP = dateTo, dateRangeP = dateRange) {
+    dateFromP.value = (mode !== "showAll") ? dateRangeP[mode].start.toISOString().slice(0,10) : '';
+    dateToP.value = (mode !== "showAll") ? dateRangeP[mode].end.toISOString().slice(0,10) : '';
+
+    emit('update:dateTo', dateToP.value)
+    emit('update:dateFrom', dateFromP.value)
+  }
+
+
+  function updateDateValue(event, emitName, reactiveElement) {
+    const newValue = event.target.value;
+    emit(emitName, newValue);
+    reactiveElement.value = newValue
+  }
 
 </script>
 
