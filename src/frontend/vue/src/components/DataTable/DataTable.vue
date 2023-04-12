06a854a90356089da84e1c334d11e7f9086f7133
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-13 01:51:49 +0200

    vue/DataTable: actionButtonHandler removed
    this is now part of "ActionMenu.functions.js"

--- src/frontend/vue/src/components/DataTable/DataTable.vue
+++ src/frontend/vue/src/components/DataTable/DataTable.vue
@@ -129,21 +129,6 @@ import PaginationBar from "../PaginationBar/PaginationBar.vue";
     actionButtonHandler(event, actionMenuVisible, actionMenu)
   }
 
-  async function actionButtonHandler(event, actionMenuVisible, actionMenuRef) {
-    actionMenuVisible.value = true;
-    await nextTick();
-    const actionButtonRect = event.target.getBoundingClientRect();
-    const actionMenuRect = actionMenuRef.value.getBoundingClientRect();
-
-    const x = (actionButtonRect.left - actionMenuRect.width + window.scrollX).toFixed();
-    const y = (actionButtonRect.top + window.scrollY).toFixed();
-
-    // Set the position for menu
-    actionMenuRef.value.style.left = `${x}px`;
-    actionMenuRef.value.style.top = `${y}px`;
-
-    actionMenuRef.value.firstElementChild.focus();
-  };
 
   watch(currentItem, () => {
     emit("updateCurrentItem", currentItem)
