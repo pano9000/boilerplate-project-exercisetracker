2a1b33feceb4306ff0b9794a67e16befa9cf0855
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-14 01:36:41 +0200

    vue/DataTable: remove left over duplicate code, now moved to ActionMenu.functions

--- src/frontend/vue/src/components/DataTable/DataTable.vue
+++ src/frontend/vue/src/components/DataTable/DataTable.vue
@@ -154,42 +154,6 @@ import { actionButtonHandler } from "../ActionMenu.functions.js";
     paginatedList.value = updatedValue
   };
 
-
-  function actionMenuDisableVisibility(actionMenuVisible) {
-    if (actionMenuVisible.value !== false) {
-      actionMenuVisible.value = false
-    }
-  }
-
-  function actionMenuHandleEscKey(event) {
-    if (event.key === "Escape") {
-      actionMenuDisableVisibility(actionMenuVisible)
-    }
-  }
-
-  function actionMenuHandleEmptyClick(event) {
-    if (!event.target.classList.contains("actionMenu_btn")) {
-      actionMenuDisableVisibility(actionMenuVisible)
-    }
-  }
-
-  const windowEventListeners = {
-    escapeKey: ["keydown", actionMenuHandleEscKey],
-    emptyClick: ["click", actionMenuHandleEmptyClick]
-  };
-
-  onMounted( () => {
-    for (const eventListener in windowEventListeners) {
-      window.addEventListener(...windowEventListeners[eventListener])
-    }
-  });
-
-  onBeforeUnmount( () => {
-    for (const eventListener in windowEventListeners) {
-      window.removeEventListener(...windowEventListeners[eventListener])
-    }
-  });
-
 </script>
 
 
