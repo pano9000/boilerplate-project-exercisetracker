94a857434d76f80b7a2cae27900aa332122a6e1e
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-18 08:58:56 +0200

    vue/stores: add a DataTableFiltersStore

---
+++ src/frontend/vue/src/stores/DataTableFilterStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useDataTableFiltersStore = defineStore("DataTableFilters", {
+
+  state: () => ({
+    filters: {}
+  }),
+
+  actions: {
+  },
+
+  getters: {
+  }
+
+});
