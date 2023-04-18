12452a2af0241b1de874d77b4706aef1a2b719d4
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-18 09:29:44 +0200

    vue/DataTableFilters/Limit: add v-bind for value

--- src/frontend/vue/src/components/DataTableFilters/Limit.vue
+++ src/frontend/vue/src/components/DataTableFilters/Limit.vue
@@ -2,6 +2,7 @@
   <label for="ui-limit_select">Limit</label>
   <select
     id="ui-limit_select"
+    :value="modelValue"
     @change="$emit('update:modelValue', $event.target.value)"
   >
     <option v-for="limit in ['Show All', 5, 10, 25, 50]" :key="limit" :value="(limit === 'Show All') ? 0 : limit">{{ limit }}</option>
