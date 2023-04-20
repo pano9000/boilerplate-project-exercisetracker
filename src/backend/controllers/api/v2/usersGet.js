b9d7f5e3f4ccd7f395ee04e26ac260d09041a965
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-20 09:27:45 +0200

    express/controllers/usersGet: add pagination support

--- src/backend/controllers/api/v2/usersGet.js
+++ src/backend/controllers/api/v2/usersGet.js
@@ -1,16 +1,28 @@
 const findDoc = require("../../../services/db/findDoc");
+const getPaginationData = require("../../../services/getPaginationData");
+const getQueryOptions = require("../../../services/getQueryOptions");
 
 async function usersGet(req, res) {
 
   try {
-    const { sortBy, sort } = req.query;
-    const sortOptions = [[sortBy || "username", sort || 1]];
 
-    const findResult = await findDoc.findAll("UserModel", null, sortOptions); //TODO: check if pagination of results should be a thing?
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const sortBy = req.query.sortBy || "username";
+    const sort = parseInt(req.query.sort) || 1;
 
-    res.status(200).json(findResult);
+    const sortOptions = [[ sortBy, sort ]];
+    const queryOptions = getQueryOptions(page, limit ||10)
+
+    const dbModelName = "UserModel"
+
+    const findResult = await findDoc.findAll(dbModelName, null, sortOptions, queryOptions);
+    const pagination = await getPaginationData(page, limit, dbModelName)
+
+    res.status(200).json({ data: findResult, pagination });
   }
   catch(error) {
+    console.log(error)
     res.status(500).json({"error": "Getting User List Failed, Please try again later"});
   }
 
