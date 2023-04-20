30c564a061ff835ecc999d7eab1ba7e3c611ff66
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-20 09:25:53 +0200

    express/validation: add pagination params to UsersGet

--- src/backend/services/validationSchemas/validation.schema.usersGet.js
+++ src/backend/services/validationSchemas/validation.schema.usersGet.js
@@ -3,5 +3,7 @@ const { UserSchema } = require("../../db/schemas");
 
 module.exports = [
   queryParams.sort,
-  queryParams.sortBy([...Object.keys(UserSchema.paths)])
+  queryParams.sortBy([...Object.keys(UserSchema.paths)]),
+  queryParams.limit,
+  queryParams.page
 ]
