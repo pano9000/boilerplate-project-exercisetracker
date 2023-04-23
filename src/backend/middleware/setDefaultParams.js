497aabc290807752ed1eb33c021b781be8fe7d8c
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-23 09:09:08 +0200

    express/middleware/setDefaultParams: wrap code in try/catch

--- src/backend/middleware/setDefaultParams.js
+++ src/backend/middleware/setDefaultParams.js
@@ -4,14 +4,20 @@ module.exports = function(dbModelName) {
 
   return (req, res, next) => {
 
-    req.query.page = parseInt(req.query.page) || 1;
-    req.query.limit = parseInt(req.query.limit) || 10;
-    req.query.sortBy = req.query.sortBy || dbModel[dbModelName]["sortByDefault"];
-    req.query.sort = parseInt(req.query.sort) || 1;
-
-    req._dbModelName = dbModelName
-
-    next()
+    try {
+      req.query.page = parseInt(req.query.page) || 1;
+      req.query.limit = parseInt(req.query.limit) || 10;
+      req.query.sortBy = req.query.sortBy || dbModel[dbModelName]["sortByDefault"];
+      req.query.sort = parseInt(req.query.sort) || 1;
+  
+      req._dbModelName = dbModelName
+  
+      next()
+    }
+    catch(error) {
+      console.log(error)
+      res.status(500).json({"error": `Unexpected Server Error`});
+    }
 
   }
 
