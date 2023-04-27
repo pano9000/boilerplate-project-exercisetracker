5e3cf8763bfb70816cfa1f8ed12740c744e8dc85
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-04-27 20:34:17 +0200

    vue/ExerciseListAll: update vue imports

--- src/frontend/vue/src/components/ExerciseListAll/ExerciseListAll.vue
+++ src/frontend/vue/src/components/ExerciseListAll/ExerciseListAll.vue
@@ -57,7 +57,7 @@
 </template>
 
 <script setup>
-  import { ref, reactive, onMounted, computed } from "vue";
+  import { ref, reactive, onMounted, watch } from "vue";
   import { getAllExercises, deleteExerciseById } from "../../services/apiEndpoints";
   import { handleApiResponse } from "../../services/apiService";
   import DataTable from "../DataTable/DataTable.vue";
