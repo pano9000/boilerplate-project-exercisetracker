166752cdae428281470d5a09ea9f0e63c33079e4
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-07 18:15:23 +0200

    vue/stores/DataListStore: add selection related getters

--- src/frontend/vue/src/stores/DataListStore.js
+++ src/frontend/vue/src/stores/DataListStore.js
@@ -14,6 +14,14 @@ export const useDataListStore = defineStore("DataList", {
   },
 
   getters: {
+
+    selectedItems: (state) => {
+      if (!Array.isArray(state.data)) return [];
+      return state.data.filter(item => item.selected === true);
+    },
+
+    allItemsSelected: (state) => (state.selectedItems.length === state.data.length) ? true : false,
+    hasSelectedItems: (state) => (state.selectedItems.length > 0) ? true : false
   }
 
 });
