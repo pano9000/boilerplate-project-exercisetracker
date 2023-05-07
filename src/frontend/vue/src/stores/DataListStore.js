2f06d6ec2ea894390fe2f25f32c48325e6f5818b
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-07 18:15:04 +0200

    vue/stores/DataListStore: fix initial type of data -> should be array

--- src/frontend/vue/src/stores/DataListStore.js
+++ src/frontend/vue/src/stores/DataListStore.js
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 export const useDataListStore = defineStore("DataList", {
 
   state: () => ({
-    data: {},
+    data: [],
     pagination: {},
     filters: {},
     currentItem: {},
