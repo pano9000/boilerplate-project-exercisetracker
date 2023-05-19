05bd198d4df8bed9cd41efc83db205c74f870291
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-19 07:26:37 +0200

    express/services/validation: add TODO comment

--- src/backend/services/validationSchemas/validation.schema.usersGet.js
+++ src/backend/services/validationSchemas/validation.schema.usersGet.js
@@ -3,7 +3,7 @@ const { UserSchema } = require("../../db/schemas");
 
 module.exports = [
   queryParams.sortOrder,
-  queryParams.sortBy([...Object.keys(UserSchema.paths)]),
+  queryParams.sortBy([...Object.keys(UserSchema.paths)]), //@TODO: potential security issue for future fields, that should not be sortable for everyone, e.g. isAdmin or similar
   queryParams.limit,
   queryParams.page,
   queryParams.query(UserSchema.obj.username.match)
