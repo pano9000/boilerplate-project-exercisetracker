e08b56588a5f9f4da1e2967c182e63faea904ff5
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 15:04:24 +0200

    vue/DataTableFilters: add Search component WIP

---
+++ src/frontend/vue/src/components/DataTableFilters/Search.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+    <label for="ui-searchFor">Search For</label>
+    <input 
+      id="ui-searchFor" 
+      type="search" 
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.value)"
+    >
+  </div>
+
+
+</template>
+
+<script setup>
+
+  defineProps(["modelValue"])
+  defineEmits(['update:modelValue'])
+
+</script>
+
+
+<style>
+
+</style>
