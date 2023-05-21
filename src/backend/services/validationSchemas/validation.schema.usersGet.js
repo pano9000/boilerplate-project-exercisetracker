a8bff7ccbeba666083fb739f966141b0b8fe4916
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 20:07:40 +0200

    express/services/validation: define schemaFields once

--- src/backend/services/validationSchemas/validation.schema.usersGet.js
+++ src/backend/services/validationSchemas/validation.schema.usersGet.js
@@ -1,9 +1,11 @@
 const { queryParams }  = require("./validation.schemaShared.params");
 const { UserSchema } = require("../../db/schemas");
 
+const userSchemaFields = [...Object.keys(UserSchema.paths)]; //@TODO: potential security issue for future fields, that should not be sortable for everyone, e.g. isAdmin or similar
+
 module.exports = [
   queryParams.sortOrder,
-  queryParams.sortBy([...Object.keys(UserSchema.paths)]), //@TODO: potential security issue for future fields, that should not be sortable for everyone, e.g. isAdmin or similar
+  queryParams.sortBy(userSchemaFields),
   queryParams.limit,
   queryParams.page,
   queryParams.searchFor(UserSchema.obj.username.match),
