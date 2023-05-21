28af8ab83731ab1ee22348a39175c96036e4431a
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 20:07:08 +0200

    express/services/validation: use searchFor/searchIn

--- src/backend/services/validationSchemas/validation.schema.usersGet.js
+++ src/backend/services/validationSchemas/validation.schema.usersGet.js
@@ -6,5 +6,6 @@ module.exports = [
   queryParams.sortBy([...Object.keys(UserSchema.paths)]), //@TODO: potential security issue for future fields, that should not be sortable for everyone, e.g. isAdmin or similar
   queryParams.limit,
   queryParams.page,
-  queryParams.query(UserSchema.obj.username.match)
+  queryParams.searchFor(UserSchema.obj.username.match),
+  queryParams.searchIn(userSchemaFields, "username"), //@TODO: some way to not hardcode that here, but maybe in the Schema instead?
 ]
