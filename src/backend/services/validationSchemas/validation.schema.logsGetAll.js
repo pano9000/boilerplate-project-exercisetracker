39066485e8373e2c78b4bf0cbeae9cb16bca9fd0
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 20:08:09 +0200

    express/services/validation: logsGetAll -> use searchFor/Searchin

--- src/backend/services/validationSchemas/validation.schema.logsGetAll.js
+++ src/backend/services/validationSchemas/validation.schema.logsGetAll.js
@@ -1,6 +1,9 @@
 const { queryParams }  = require("./validation.schemaShared.params")
 const { ExerciseSchema } = require("../../db/schemas")
 
+const exerciseSchemaFields = [...Object.keys(ExerciseSchema.paths)]; //@TODO: potential security issue for future fields, that should not be sortable for everyone, e.g. isAdmin or similar
+
+
 const logsGetAllSchema = [
 
   queryParams.from,
@@ -8,8 +11,9 @@ const logsGetAllSchema = [
   queryParams.limit,
   queryParams.page,
   queryParams.sortOrder,
-  queryParams.sortBy([...Object.keys(ExerciseSchema.paths)])
-
+  queryParams.sortBy(exerciseSchemaFields),
+  queryParams.searchFor(/[a-zA-Z0-9]/),
+  queryParams.searchIn(exerciseSchemaFields, "description"), //@TODO: some way to not hardcode that here, but maybe in the Schema instead?
 ]
 
 module.exports = logsGetAllSchema
