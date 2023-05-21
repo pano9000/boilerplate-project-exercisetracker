7f91b3a6e918b7b139a2020f5ea30ff186c31d4e
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 17:17:12 +0200

    vue/DashBoardInfoCard: add new component

---
+++ src/frontend/vue/src/components/DashboardInfoCard.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="ui-dashboard-card_wrap">
+    <div class="ui-dashboard-card_heading">
+      <slot name="cardHeading"></slot>
+    </div>
+    <div class="ui-dashboard-card_description">
+      <slot name="cardDescription"></slot>
+    </div>
+  </div>
+
+
+</template>
+
+<script setup>
+</script>
+
+<style>
+.ui-dashboard-card_wrap {
+  background-color: #069;
+  padding: 1rem;
+  border-radius: var(--border-radius);
+  box-shadow: var(--box-shadow);
+}
+
+.ui-dashboard-card_heading {
+  font-size: 2rem;
+  font-weight: 600;
+}
+</style>
