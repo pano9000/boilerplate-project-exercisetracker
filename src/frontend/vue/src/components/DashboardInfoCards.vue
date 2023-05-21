3b3c881eea635da57c3ac11f24ebefac627515ab
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-05-21 17:17:52 +0200

    vue/DashBoardInfoCards: add new "wrapping" component

---
+++ src/frontend/vue/src/components/DashboardInfoCards.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ui-dashboard-cards">
+
+    <slot></slot>
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+
+.ui-dashboard-cards {
+  display: flex;
+}
+</style>
